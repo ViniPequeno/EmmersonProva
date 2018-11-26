3cc2f7ed6f39d29741dff1e459a6935296ef0f61
Pedro Pequeno <viniciuspedro350@gmail.com>
2018-11-26 12:04:02 -0200

    Commit

---
+++ app/src/main/java/com/fvncional/developer/emmersonprova/Database/AppDatabase.java
@@ -0,0 +1,20 @@
+package com.fvncional.developer.emmersonprova.Database;
+
+import com.fvncional.developer.emmersonprova.DAO.AlunoDAO;
+import com.fvncional.developer.emmersonprova.DAO.DisciplinaDAO;
+import com.fvncional.developer.emmersonprova.DAO.NotaDAO;
+import com.fvncional.developer.emmersonprova.DAO.Professor;
+import com.fvncional.developer.emmersonprova.Models.Aluno;
+import com.fvncional.developer.emmersonprova.Models.Disciplina;
+import com.fvncional.developer.emmersonprova.Models.Nota;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Professor.class, Aluno.class, Disciplina.class, Nota.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract AlunoDAO alunoDAO();
+    public abstract DisciplinaDAO disciplinaDAO();
+    public abstract NotaDAO notaDAO();
+    public abstract Professor professorDAO();
+}
--- app/src/main/java/com/fvncional/developer/emmersonprova/MainActivity.java
+++ app/src/main/java/com/fvncional/developer/emmersonprova/MainActivity.java
@@ -3,11 +3,19 @@ package com.fvncional.developer.emmersonprova;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.fvncional.developer.emmersonprova.Database.AppDatabase;
+
+import androidx.room.Room;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
     }
 }
